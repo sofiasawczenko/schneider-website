a3fc100c5ac117f6d32a0941c47f432efc4b3f80
bellujrb <bellujrb@gmail.com>
2023-09-30 23:14:16 -0300

    fix

--- js/drawer.js
+++ js/drawer.js
@@ -3,7 +3,7 @@ const burger = document.getElementById('burger');
 const overlay = document.getElementById('overlay');
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    nav.classList.toggle('active');
-    overlay.classList.toggle('active');
+    burger.classList.toggle('activesuper');
+    nav.classList.toggle('activesuper');
+    overlay.classList.toggle('overlay');
 });
